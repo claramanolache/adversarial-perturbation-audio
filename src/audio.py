0ec89ab24e6f170e1203b639c866c84895ea2dc2
Clara Manolache <clara.manolache@gmail.com>
2026-06-16 12:58:45 -0700

    test file + some new classes installed to help (debugging rn)

--- src/audio.py
+++ src/audio.py
@@ -1,4 +1,7 @@
+import os
 from pydub import AudioSegment
+
+# https://github.com/jiaaro/pydub
 
 file_path = "/Users/claramanolache/adversarial-perturbation-audio/res/mama_youve_been_on_my_mind.mp3"
 song = AudioSegment.from_mp3(file_path)
